0ad0aa15bee610f0fb01a45b4146e49e83de68c2
Aurora <949244762@qq.com>
2023-05-12 14:44:14 +0800

    add domain

--- project-api/main.go
+++ project-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	_ "test.com/project-api/api"
@@ -15,5 +16,7 @@ func main() {
 	r.Use(midd.RequestLog())
 	r.StaticFS("/upload", http.Dir("upload"))
 	router.InitRouter(r)
+	// 开启pprof 默认访问路径是/debug/pprof 或自定义 例如 pprof.Register(r, "/info/pprof") 路径即为 /info/pprof
+	pprof.Register(r)
 	srv.Run(r, config.C.SC.Name, config.C.SC.Addr, nil)
 }
