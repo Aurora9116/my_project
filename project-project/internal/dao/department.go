58395790baa396b7a0a932e031abe58df37dbeef
Aurora <949244762@qq.com>
2023-05-13 15:25:54 +0800

    update

--- project-project/internal/dao/department.go
+++ project-project/internal/dao/department.go
@@ -13,10 +13,10 @@ type DepartmentDao struct {
 
 func (d *DepartmentDao) ListDepartment(organizationCode int64, parentDepartmentCode int64, page int64, size int64) (list []*data.Department, total int64, err error) {
 	session := d.conn.Default(context.Background())
-	session.Model(&data.Department{})
-	session.Where("organization_code=?", organizationCode)
+	session = session.Model(&data.Department{})
+	session = session.Where("organization_code=?", organizationCode)
 	if parentDepartmentCode > 0 {
-		session.Where("pcode=?", parentDepartmentCode)
+		session = session.Where("pcode=?", parentDepartmentCode)
 	}
 	err = session.Count(&total).Error
 	err = session.Limit(int(size)).Offset(int((page - 1) * size)).Find(&list).Error
@@ -30,9 +30,9 @@ func (d *DepartmentDao) Save(dpm *data.Department) error {
 
 func (d *DepartmentDao) FindDepartment(ctx context.Context, organizationCode int64, parentDepartmentCode int64, name string) (*data.Department, error) {
 	session := d.conn.Default(ctx)
-	session.Model(&data.Department{}).Where("organization_code=? AND name=?", organizationCode, name)
+	session = session.Model(&data.Department{}).Where("organization_code=? AND name=?", organizationCode, name)
 	if parentDepartmentCode > 0 {
-		session.Where("pcode=?", parentDepartmentCode)
+		session = session.Where("pcode=?", parentDepartmentCode)
 	}
 	var dp *data.Department
 	err := session.Limit(1).Take(&dp).Error
